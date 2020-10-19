1c88e2aeaa568fa45968e1ce81aad8a0f382d3d4
yassine zahrouni <ge56dan@mytum.de>
2020-10-19 14:51:54 +0200

    --- Auto-Git Commit ---

---
+++ EN 13001-3-3/Anhang.py
@@ -0,0 +1,44 @@
+Laufradmaterial1 = {
+    "Bezeichnung": "",
+    "Norm" : "",
+    "Materialnummer" : "",
+    "Lieferzustand": "",
+    "Bruchfestigkeit": ,
+    "Aulegungshärte":
+}
+Laufradmaterial2 = {
+    "Bezeichnung": "",
+    "Norm" : "",
+    "Materialnummer" : "",
+    "Lieferzustand": "",
+    "Bruchfestigkeit": ,
+    "Aulegungshärte":
+}
+
+Laufradmaterial3 = {
+    "Bezeichnung": "",
+    "Norm" : "",
+    "Materialnummer" : "",
+    "Lieferzustand": "",
+    "Bruchfestigkeit": ,
+    "Aulegungshärte":
+}
+
+Laufradmaterial4 = {
+    "Bezeichnung": "",
+    "Norm" : "",
+    "Materialnummer" : "",
+    "Lieferzustand": "",
+    "Bruchfestigkeit": ,
+    "Aulegungshärte":
+}
+
+Laufradmaterial5 = {
+    "Bezeichnung": "",
+    "Norm" : "",
+    "Materialnummer" : "",
+    "Lieferzustand": "",
+    "Bruchfestigkeit": ,
+    "Aulegungshärte":
+}
+
