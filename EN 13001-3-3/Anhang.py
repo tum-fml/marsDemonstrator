f3de45e68c819123721dfbdae2c3b69315a51ab0
yassine zahrouni <ge56dan@mytum.de>
2020-10-20 08:47:27 +0200

    --- Auto-Git Commit ---

--- EN 13001-3-3/Anhang.py
+++ EN 13001-3-3/Anhang.py
@@ -1,60 +1,72 @@
 Laufradmaterial1 = {
     "Bezeichnung": "GE 300",
     "Norm" : "EN 10293",
-    "Materialnummer" : "0.7060",
-    "Lieferzustand": "Gusszustand",
-    "Bruchfestigkeit":  ,
-    "Auslegungshärte": 
+    "Materialnummer" : 1.0558,
+    "Lieferzustand": "+N",
+    "Bruchfestigkeit": 520 ,
+    "Auslegungshärte": 155
 }
 Laufradmaterial2 = {
-    "Bezeichnung": "",
-    "Norm" : "",
-    "Materialnummer" : "",
-    "Lieferzustand": "",
-    "Bruchfestigkeit": ,
-    "Auslegungshärte":
+    "Bezeichnung": "EN-GJS 600-3",
+    "Norm" : "EN 1563",
+    "Materialnummer" : 0.7060 ,
+    "Lieferzustand": "Gusszustand",
+    "Bruchfestigkeit": 600 ,
+    "Auslegungshärte": 210
 }
 
 Laufradmaterial3 = {
-    "Bezeichnung": "",
-    "Norm" : "",
-    "Materialnummer" : "",
-    "Lieferzustand": "",
-    "Bruchfestigkeit": ,
-    "Auslegungshärte":
+    "Bezeichnung": "EN-GJS-700-2",
+    "Norm" : "EN 1563",
+    "Materialnummer" : 0.7070,
+    "Lieferzustand": "Gusszustand",
+    "Bruchfestigkeit":700 ,
+    "Auslegungshärte": 245
 }
 
 Laufradmaterial4 = {
-    "Bezeichnung": "",
-    "Norm" : "",
-    "Materialnummer" : "",
-    "Lieferzustand": "",
-    "Bruchfestigkeit": ,
-    "Auslegungshärte":
+    "Bezeichnung": "25CrMo4",
+    "Norm" : "EN 10083-3",
+    "Materialnummer" : 1.7218,
+    "Lieferzustand": "+QT",
+    "Bruchfestigkeit": 650 ,
+    "Auslegungshärte": 190
 }
 
 Laufradmaterial5 = {
-    "Bezeichnung": "",
-    "Norm" : "",
-    "Materialnummer" : "",
-    "Lieferzustand": "",
-    "Bruchfestigkeit": ,
-    "Auslegungshärte":
+    "Bezeichnung": "34CrMo4",
+    "Norm" : "EN 10083-3",
+    "Materialnummer" : 1.7220,
+    "Lieferzustand": "+QT",
+    "Bruchfestigkeit": 700 ,
+    "Auslegungshärte": 210
 }
 
 Laufradmaterial6 = {
-    "Bezeichnung": "",
-    "Norm" : "",
-    "Materialnummer" : "",
-    "Lieferzustand": "",
-    "Bruchfestigkeit": ,
-    "Auslegungshärte":
+    "Bezeichnung": "42CrMo4",
+    "Norm" : "EN 10083-3",
+    "Materialnummer" : 1.7225,
+    "Lieferzustand": "+QT",
+    "Bruchfestigkeit": 750 ,
+    "Auslegungshärte": 225
 }
 Laufradmaterial7 = {
-    "Bezeichnung": "",
-    "Norm" : "",
-    "Materialnummer" : "",
-    "Lieferzustand": "",
-    "Bruchfestigkeit": ,
-    "Auslegungshärte":
+    "Bezeichnung": "33NiCrMoV14-5",
+    "Norm" : "EN 10280-3",
+    "Materialnummer" : 1.6956,
+    "Lieferzustand": "+QT",
+    "Bruchfestigkeit": 1000 ,
+    "Auslegungshärte": 295
+}
+
+
+Laufradmaterialien = {
+   "Laufradmaterial1" = Laufradmaterial1, 
+   "Laufradmaterial2" : Laufradmaterial2,
+   "Laufradmaterial3" : Laufradmaterial3,
+   "Laufradmaterial4" : Laufradmaterial4,
+   "Laufradmaterial5" : Laufradmaterial5,
+   "Laufradmaterial6"
+   "Laufradmaterial7"
+
 }
