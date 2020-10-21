c4c6ef0a9b6efedb75fcfd9044443b28609469eb
yassine zahrouni <ge56dan@mytum.de>
2020-10-21 09:40:58 +0200

    --- Auto-Git Commit ---

--- EN 13001-3-3/Input.py
+++ EN 13001-3-3/Input.py
@@ -1,23 +1,3 @@
 import tkinter as tk
 
-window = tk.Tk()
-window.title("Address Entry Form")
-
-frame1 = tk.Frame(master=window)
-frame1["relief"] = tk.SUNKEN
-frame1["borderwidth"] = 3
-frame1.pack(fill=tk.X)
-
-frame2 = tk.Frame(master=window)
-frame2.pack(fill=tk.X)
-
-list = {
-    0: "Bezeichnung",
-    1: "Last Name",
-    2: "Address Line 1",
-    3: "Address Line 2",
-    4: "City",
-    5: "State/Province",
-    6: "Postal Code",
-    7: "Country",
-}
+root = tk()
