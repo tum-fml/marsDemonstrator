d8b53faf3cc0df823ff9b4b785d2bdc7cd13f8b0
yassine zahrouni <ge56dan@mytum.de>
2020-10-21 13:42:30 +0200

    --- Auto-Git Commit ---

--- EN 13001-3-3/Input.py
+++ EN 13001-3-3/Input.py
@@ -4,8 +4,8 @@ import tkinter
 input = tkinter.Tk()
 input.title("Eigenschaften des Rad-Schiene Systems")
 input.minsize(600,450)
-label_welcome = tkinter(input, Text="Welcome to MARS")
-
+label_welcome = tkinter.Label(input, text="Welcome to MARS")
+label_welcome.pack()
 #input.geometry("800x600")
 
 input.mainloop()
