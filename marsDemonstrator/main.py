e9af742b1e2047a15b04b25c271a789ab580d37c
Mathias <ge67kub@mytum.de>
2021-02-11 13:04:02 +0100

    test

--- marsDemonstrator/main.py
+++ marsDemonstrator/main.py
@@ -1,6 +1,5 @@
-from open_interface import build_graphical_interface
-
+from open_interface_copy import MarsGUI()
 
 if __name__ == "__main__":
-    mars_interface = build_graphical_interface()
-    mars_interface.mainloop()
+    gui = MarsGUI()
+    gui.mainloop()
